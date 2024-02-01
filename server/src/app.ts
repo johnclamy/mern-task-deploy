283173d175a7c963854666d9347113e833ca715a
John_Lamy <johnclamy@gmail.com>
2024-02-01 13:41:37 +0000

    query the MongoDB tasks collection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import TaskModel from "./models/task";
 
 const app = express();
 
 // home page
-app.get("/", (req, res) => res.send("Welcome to the home page"));
+app.get("/", async (req, res) => {
+  const tasks = await TaskModel.find().exec();
+  res.status(200).json(tasks);
+});
 
 export default app;
