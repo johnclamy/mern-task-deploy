cab9c0c1bfbdc61788726fc8bfd4cb38dc86e7b8
John_Lamy <johnclamy@gmail.com>
2024-02-02 22:52:30 +0000

    handle  getting a single task from the db

--- server/src/controllers/tasks.ts
+++ server/src/controllers/tasks.ts
@@ -9,3 +9,30 @@ export const getTasks: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getTask: RequestHandler = async (req, res, next) => {
+  const taskId = req.params.taskId;
+
+  try {
+    const task = await TaskModel.findById(taskId).exec();
+    res.status(200).json(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createTask: RequestHandler = async (req, res, next) => {
+  const title = req.body.title;
+  const text = req.body.text;
+
+  try {
+    const newTask = await TaskModel.create({
+      title,
+      text,
+    });
+
+    res.status(201).json(newTask);
+  } catch (err) {
+    next(err);
+  }
+};
