6c14c0cb2d93e9aa196bff9b0908deaa09809d66
John_Lamy <johnclamy@gmail.com>
2024-02-02 14:36:32 +0000

    add the middleware for the tasks endpoint to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,27 @@
 import "dotenv/config";
-import express from "express";
-import TaskModel from "./models/task";
+import express, { NextFunction, Request, Response } from "express";
+import tasksRoutes from "./routes/tasks";
 
 const app = express();
 
-// home page
-app.get("/", async (req, res) => {
-  const tasks = await TaskModel.find().exec();
-  res.status(200).json(tasks);
+app.use("/api/tasks", tasksRoutes);
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
 });
+
+app.use(
+  (err: unknown, req: Request, res: Response, next: NextFunction): void => {
+    console.error(err);
+    let statusCode = 500;
+    let errorMessage = "An unknown error occurred";
+
+    if (err instanceof Error) {
+      errorMessage = err.message;
+    }
+
+    res.status(statusCode).json({ err: errorMessage });
+  }
+);
 
 export default app;
