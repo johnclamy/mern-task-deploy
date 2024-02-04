de3a596e3b71ae4677fd2f648cf791afc53a4db4
John_Lamy <johnclamy@gmail.com>
2024-02-04 00:02:27 +0000

    setup the route for a single task

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,28 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import tasksRoutes from "./routes/tasks";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(express.json());
 app.use("/api/tasks", tasksRoutes);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: unknown, req: Request, res: Response, next: NextFunction): void => {
     console.error(err);
     let statusCode = 500;
     let errorMessage = "An unknown error occurred";
 
-    if (err instanceof Error) {
+    if (isHttpError(err)) {
+      statusCode = err.status;
       errorMessage = err.message;
     }
 
--- server/src/routes/tasks.ts
+++ server/src/routes/tasks.ts
@@ -4,5 +4,7 @@ import * as TasksController from "../controllers/tasks";
 const router = express.Router();
 
 router.get("/", TasksController.getTasks);
+router.get("/:taskId", TasksController.getTask);
+router.post("/", TasksController.createTask);
 
 export default router;
