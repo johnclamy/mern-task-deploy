8fbb8235f55dbd5e0aef8ec220389f86f04471a3
John_Lamy <johnclamy@gmail.com>
2024-02-04 16:48:35 +0000

    define the route endpoint for updating a task

--- server/src/routes/tasks.ts
+++ server/src/routes/tasks.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get("/", TasksController.getTasks);
 router.get("/:taskId", TasksController.getTask);
 router.post("/", TasksController.createTask);
+router.patch("/:taskId", TasksController.updateTask);
 
 export default router;
