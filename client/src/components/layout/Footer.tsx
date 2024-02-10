cb68628edccda629edee6557f13dedf541100f66
John_Lamy <johnclamy@gmail.com>
2024-02-10 20:39:31 +0000

    setup a footer component

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => (
+  <footer style={{ marginTop: "auto" }}>
+    <p className=" my-3 text-center small text-black-50">
+      Open source {new Date().getFullYear()} <strong>MIT</strong> licence MERN
+      stack app
+    </p>
+  </footer>
+);
+
+export default Footer;
