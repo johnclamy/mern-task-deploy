4809b32c4dac451cc3332e0217d135a524c0bbd3
John_Lamy <johnclamy@gmail.com>
2024-02-11 22:09:42 +0000

    set  a fetch tasks func that return a promise

---
+++ client/src/components/api/fetchTasksPromise.ts
@@ -0,0 +1,10 @@
+import fetchData from "./fetch-data";
+import { Task } from "../../models/task";
+
+const END_POINT = "/api/tasks";
+
+export default async function fetchTasksPromise(): Promise<Task[]> {
+  const res = await fetchData(END_POINT, { method: "GET" });
+
+  return res.json();
+}
