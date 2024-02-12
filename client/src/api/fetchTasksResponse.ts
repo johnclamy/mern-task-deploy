1e4f805644ce5d5029948057e2cf5c46e91dd297
John_Lamy <johnclamy@gmail.com>
2024-02-12 23:07:19 +0000

    change file name to fetchTasksResponse.tsx

--- client/src/api/fetchTasksResponse.ts
+++ client/src/api/fetchTasksResponse.ts
@@ -1,9 +1,9 @@
 import fetchData from "./fetch-data";
 import { Task } from "../models/task";
 
-const END_POINT = "/api/tasks";
+export const END_POINT = "/api/tasks";
 
-export default async function fetchTasksPromise(): Promise<Task[]> {
+export default async function fetchTasksResponse(): Promise<Task[]> {
   const res = await fetchData(END_POINT, { method: "GET" });
 
   return res.json();
