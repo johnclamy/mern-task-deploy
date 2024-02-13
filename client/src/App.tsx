abba43e347b41c4e1722a48bdf0abc93826c91b5
John_Lamy <johnclamy@gmail.com>
2024-02-13 00:58:32 +0000

    add the modal to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import { Task as TaskModel } from "./models/task";
-import fetchTasksPromise from "./api/fetchTasksPromise";
+import fetchTasksResponse from "./api/fetchTasksResponse";
 import Tasks from "./components/app/Tasks";
+import CreateTaskModal from "./components/app/CreateTaskModal";
 import Main from "./components/layout/Main";
 import TopBar from "./components/layout/TopBar";
 import Footer from "./components/layout/Footer";
+import HeroButton from "./components/widgets/HeroButton";
 
 function App() {
   const [tasks, setTasks] = useState<TaskModel[]>([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleHide = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
   const fetchTasks = async () => {
     try {
-      const tasks = await fetchTasksPromise();
+      const tasks = await fetchTasksResponse();
       setTasks(tasks);
     } catch (err) {
       console.error(err);
@@ -29,7 +35,11 @@ function App() {
       <section>
         <TopBar />
         <Main>
-          <Tasks tasks={tasks} />
+          <div>
+            <HeroButton onShow={handleShow}>Add a task</HeroButton>
+            <CreateTaskModal show={showModal} onHide={handleHide} />
+            <Tasks tasks={tasks} />
+          </div>
         </Main>
       </section>
       <Footer />
