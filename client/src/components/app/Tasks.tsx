34c46ec9d59204f186996700615a680828ab3409
John_Lamy <johnclamy@gmail.com>
2024-02-19 20:39:20 +0000

    prop drill onDeleteTask

--- client/src/components/app/Tasks.tsx
+++ client/src/components/app/Tasks.tsx
@@ -6,14 +6,15 @@ import Task from "./Task";
 
 type TasksProps = {
   tasks: TaskModel[];
+  onDeleteTask: (task: TaskModel) => void;
 };
 
-const Tasks = ({ tasks }: TasksProps) => (
+const Tasks = ({ tasks, onDeleteTask }: TasksProps) => (
   <Container>
     <Row xs={1} md={2} lg={3}>
       <Col>
         {tasks.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task key={task._id} task={task} onDeleteTask={onDeleteTask} />
         ))}
       </Col>
     </Row>
