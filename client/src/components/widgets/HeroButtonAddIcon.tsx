a9eb1e7af19934e62f8780a52239bccecf60f6ee
John_Lamy <johnclamy@gmail.com>
2024-02-28 20:49:11 +0000

    create an icon for the add btn

---
+++ client/src/components/widgets/HeroButtonAddIcon.tsx
@@ -0,0 +1,13 @@
+import { MdAdd } from "react-icons/md";
+
+const HeroButtonAddIcon = () => (
+  <MdAdd
+    style={{
+      marginRight: "0.2rem",
+      fontWeight: "bolder",
+      fontSize: "25px",
+    }}
+  />
+);
+
+export default HeroButtonAddIcon;
